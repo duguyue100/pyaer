de37526bfb102fbf2ed5ebc0cf2fc53dfbafcc55
Yuhuang Hu <duguyue100@gmail.com>
2017-09-29 15:01:25 +0200

    update a dvs128 example

---
+++ Makefile
@@ -0,0 +1,23 @@
+# This is a Python template Makefile, do modification as you want
+#
+# Project:
+# Author:
+# Email :
+
+HOST = 127.0.0.1
+PYTHONPATH="$(shell printenv PYTHONPATH):$(PWD)"
+
+clean:
+	find . -name '*.pyc' -exec rm --force {} +
+	find . -name '*.pyo' -exec rm --force {} +
+	find . -name '*~' -exec rm --force  {} +
+
+run:
+
+test:
+	PYTHONPATH=$(PYTHONPATH) python
+
+dvs128-test:
+	PYTHONPATH=$(PYTHONPATH) python ./pyaer/scripts/dvs128_test.py
+
+cleanall:
---
+++ pyaer/scripts/dvs128_test.py
@@ -0,0 +1,23 @@
+"""DVS128 Test.
+
+Author: Yuhuang Hu
+Email : duguyue100@gmail.com
+"""
+
+from pyaer import libcaer
+
+handle = libcaer.caerDeviceOpen(
+    1, libcaer.CAER_DEVICE_DVS128, 0, 0, "")
+
+info = libcaer.caerDVS128InfoGet(handle)
+
+#  print("device ID: "+str(libcaer.caer_dvs128_info_deviceID_get(info)))
+
+#  if(libcaer.caer_dvs128_info_deviceIsMaster_get(info)):
+#      print("device is Master: ")
+#  else:
+#      print("device is Slave")
+#
+#  print("device Serial Number"+str(libcaer.caer_dvs128_info_deviceSerialNumber_get(info)))
+#
+#  print(libcaer.caer_dvs128_info_deviceString_get(info))
