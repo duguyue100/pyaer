df97cce547b7e59f990931e45816f4aa1d0f3a59
Yuhuang Hu <duguyue100@gmail.com>
2017-10-05 22:12:35 +0200

    update dvs

---
+++ pyaer/davis.py
@@ -0,0 +1,21 @@
+"""DAVIS Camera.
+
+Author: Yuhuang Hu
+Email : duguyue100@gmail.com
+"""
+from pyaer import libcaer
+
+
+class DAVIS(object):
+    """Class for managing single DAVIS device."""
+    def __init__(self,
+                 device_id=1,
+                 bus_number_restrict=0,
+                 dev_address_restrict=0,
+                 serial_number=""):
+        """DAVIS.
+
+        Parameters
+        ----------
+        """
+        pass
