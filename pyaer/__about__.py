954befd53ea863b31a25511c2cd10c8b3dd64ae2
Yuhuang Hu <duguyue100@gmail.com>
2017-12-13 15:19:16 +0100

    update version for marking travis changes

--- pyaer/__about__.py
+++ pyaer/__about__.py
@@ -8,7 +8,7 @@ from __future__ import division, print_function, unicode_literals
 
 __all__ = ("__version__", "__author__", "__author_email__", "__url__")
 
-__version__ = "0.1.0a5"
+__version__ = "0.1.0a6"
 __author__ = "Yuhuang Hu"
 __author_email__ = "duguyue100@gmail.com"
 __url__ = "https://github.com/duguyue100/pyaer"
