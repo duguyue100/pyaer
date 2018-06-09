cf0752e57c27afff646b7f767ac92703bf69b430
Yuhuang Hu <duguyue100@gmail.com>
2018-06-09 02:25:52 +0200

    unify the names

--- pyaer/filters.py
+++ pyaer/filters.py
@@ -22,96 +22,105 @@ class DVSNoise(object):
         size_y : uint16
             maximum Y axis resolution.
         """
-        self.handle = libcaer.caerFilterDVSNoiseInitialize(
-                sizeX=size_x, sizeY=size_y)
+        self.size_x = size_x
+        self.size_y = size_y
+        self.handle = None
+
+        self.initialize()
 
         if self.handle is None:
             raise ValueError("Software background activity filter"
                              "initialization failed.")
 
+    def initialize(self):
+        """Initialize."""
+        if self.handle is None:
+            self.handle = libcaer.caerFilterDVSNoiseInitialize(
+                    sizeX=self.size_x, sizeY=self.size_y)
+
     def destroy(self):
         """Destroy DVS noise filter to free up memory."""
         libcaer.caerFilterDVSNoiseDestroy(self.handle)
 
-    def set_configs(self, config_obj):
+    def set_bias(self, bias_obj):
         """Configure filter.
 
         Parameters
         ----------
-        config_obj : dict
+        bias_obj : dict
             A dictionary that contains the configuration of
             the filter
         """
         self.set_config(
             libcaer.CAER_FILTER_DVS_BACKGROUND_ACTIVITY_TWO_LEVELS,
-            config_obj["sw_background_activity_two_levels"])
+            bias_obj["sw_background_activity_two_levels"])
         self.set_config(
             libcaer.CAER_FILTER_DVS_BACKGROUND_ACTIVITY_SUPPORT_MIN,
-            config_obj["sw_background_activity_support_min"])
+            bias_obj["sw_background_activity_support_min"])
         self.set_config(
             libcaer.CAER_FILTER_DVS_BACKGROUND_ACTIVITY_SUPPORT_MAX,
-            config_obj["sw_background_activity_support_max"])
+            bias_obj["sw_background_activity_support_max"])
         self.set_config(
             libcaer.CAER_FILTER_DVS_BACKGROUND_ACTIVITY_TIME,
-            config_obj["sw_background_activity_time"])
+            bias_obj["sw_background_activity_time"])
         self.set_config(
             libcaer.CAER_FILTER_DVS_BACKGROUND_ACTIVITY_ENABLE,
-            config_obj["sw_background_activity_enable"])
+            bias_obj["sw_background_activity_enable"])
 
         self.set_config(
             libcaer.CAER_FILTER_DVS_REFRACTORY_PERIOD_TIME,
-            config_obj["sw_refractory_period_time"])
+            bias_obj["sw_refractory_period_time"])
         self.set_config(
             libcaer.CAER_FILTER_DVS_REFRACTORY_PERIOD_ENABLE,
-            config_obj["sw_refractory_period_enable"])
+            bias_obj["sw_refractory_period_enable"])
 
         self.set_config(
             libcaer.CAER_FILTER_DVS_HOTPIXEL_ENABLE,
-            config_obj["sw_hotpixel_enable"])
+            bias_obj["sw_hotpixel_enable"])
 
-        if config_obj["sw_hotpixel_enable"] is True:
+        if bias_obj["sw_hotpixel_enable"] is True:
             self.set_config(
                 libcaer.CAER_FILTER_DVS_HOTPIXEL_LEARN,
-                config_obj["sw_hotpixel_learn"])
+                bias_obj["sw_hotpixel_learn"])
             self.set_config(
                 libcaer.CAER_FILTER_DVS_HOTPIXEL_TIME,
-                config_obj["sw_hotpixel_time"])
+                bias_obj["sw_hotpixel_time"])
             self.set_config(
                 libcaer.CAER_FILTER_DVS_HOTPIXEL_COUNT,
-                config_obj["sw_hotpixel_count"])
+                bias_obj["sw_hotpixel_count"])
 
-    def get_configs(self):
+    def get_bias(self):
         """Export configuration."""
-        config_obj = {}
+        bias_obj = {}
 
-        config_obj["sw_background_activity_two_levels"] = self.get_config(
+        bias_obj["sw_background_activity_two_levels"] = self.get_config(
             libcaer.CAER_FILTER_DVS_BACKGROUND_ACTIVITY_TWO_LEVELS)
-        config_obj["sw_background_activity_support_min"] = self.get_config(
+        bias_obj["sw_background_activity_support_min"] = self.get_config(
             libcaer.CAER_FILTER_DVS_BACKGROUND_ACTIVITY_SUPPORT_MIN)
-        config_obj["sw_background_activity_support_max"] = self.get_config(
+        bias_obj["sw_background_activity_support_max"] = self.get_config(
             libcaer.CAER_FILTER_DVS_BACKGROUND_ACTIVITY_SUPPORT_MAX)
-        config_obj["sw_background_activity_time"] = self.get_config(
+        bias_obj["sw_background_activity_time"] = self.get_config(
             libcaer.CAER_FILTER_DVS_BACKGROUND_ACTIVITY_TIME)
-        config_obj["sw_background_activity_enable"] = self.get_config(
+        bias_obj["sw_background_activity_enable"] = self.get_config(
             libcaer.CAER_FILTER_DVS_BACKGROUND_ACTIVITY_ENABLE)
 
-        config_obj["sw_refractory_period_time"] = self.get_config(
+        bias_obj["sw_refractory_period_time"] = self.get_config(
             libcaer.CAER_FILTER_DVS_REFRACTORY_PERIOD_TIME)
-        config_obj["sw_refractory_period_enable"] = self.get_config(
+        bias_obj["sw_refractory_period_enable"] = self.get_config(
             libcaer.CAER_FILTER_DVS_REFRACTORY_PERIOD_ENABLE)
 
-        config_obj["sw_hotpixel_enable"] = self.get_config(
+        bias_obj["sw_hotpixel_enable"] = self.get_config(
             libcaer.CAER_FILTER_DVS_HOTPIXEL_ENABLE)
 
-        config_obj["sw_hotpixel_learn"] = self.get_config(
+        bias_obj["sw_hotpixel_learn"] = self.get_config(
             libcaer.CAER_FILTER_DVS_HOTPIXEL_LEARN)
-        if config_obj["sw_hotpixel_enable"] is True:
-            config_obj["sw_hotpixel_time"] = self.get_config(
+        if bias_obj["sw_hotpixel_enable"] is True:
+            bias_obj["sw_hotpixel_time"] = self.get_config(
                 libcaer.CAER_FILTER_DVS_HOTPIXEL_TIME)
-            config_obj["sw_hotpixel_count"] = self.get_config(
+            bias_obj["sw_hotpixel_count"] = self.get_config(
                 libcaer.CAER_FILTER_DVS_HOTPIXEL_COUNT)
 
-        return config_obj
+        return bias_obj
 
     def set_config(self, param_addr, param):
         """Set configuration.
