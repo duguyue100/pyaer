ced331c74c2e607b87bca757e419d125bbf0fd99
Yuhuang Hu <duguyue100@gmail.com>
2018-11-13 14:42:32 +0100

    update udev rules

--- install-udev.sh
+++ install-udev.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-touch /tmp/65-inilabs.rules
+touch /tmp/65-inivation.rules
 echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="152a", ATTR{idProduct}=="84[0-1]?", MODE="0666"
-' >> /tmp/65-inilabs.rules
-echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="0403", ATTR{idProduct}=="6014", MODE="0666"' >> /tmp/65-inilabs.rules
+' >> /tmp/65-inivation.rules
+echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="0403", ATTR{idProduct}=="6014", MODE="0666"' >> /tmp/65-inivation.rules
 
-# copied from https://github.com/uzh-rpg/rpg_dvs_ros
+touch /tmp/66-inivation_devel.rules
+echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="1134", ATTR{idProduct}=="8001", MODE="0666"
+' >> /tmp/66-inivation_devel.rules
+echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="04b4", ATTR{idProduct}=="8613", MODE="0666"
+' >> /tmp/66-inivation_devel.rules
+echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="04b4", ATTR{idProduct}=="0053", MODE="0666"
+' >> /tmp/66-inivation_devel.rules
+echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="04b4", ATTR{idProduct}=="00f3", MODE="0666"' >> /tmp/66-inivation_devel.rules
+
+# copied from https://gitlab.com/inivation/libcaer
 echo "Copying udev rule (needs root privileges)."
-sudo cp /tmp/65-inilabs.rules /etc/udev/rules.d/
+sudo cp /tmp/65-inivation.rules /etc/udev/rules.d/
+sudo cp /tmp/66-inivation_devel.rules /etc/udev/rules.d/
 
 echo "Reloading udev rules."
 sudo udevadm control --reload-rules
