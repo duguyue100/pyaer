083a3cf551a03ae84f79b53d47cea5e332735504
Yuhuang Hu <duguyue100@gmail.com>
2020-11-30 17:48:19 +0100

    add packing and unpacking for davis and dvs128

--- pyaer/comm.py
+++ pyaer/comm.py
@@ -14,9 +14,11 @@ from __future__ import print_function, absolute_import
 
 import os
 import time
+import json
 import subprocess
-import zmq
 from threading import Thread, Event
+import numpy as np
+import zmq
 
 
 class EventPublisher(object):
@@ -56,6 +58,69 @@ class EventPublisher(object):
         """
         return data
 
+    def pack_np_array(self, data_array):
+        """Pack numpy array for sending.
+
+        # Arguments
+            data_array: numpy.ndarray
+                the numpy array to append.
+
+        # Returns
+            packed_data_array: list
+                The array and its data type, shape information.
+        """
+        if data_array is None:
+            return [b'None', b'None']
+
+        md = dict(
+            dtype=str(data_array.dtype),
+            shape=data_array.shape)
+
+        return [data_array, bytes(json.dumps(md), "utf-8")]
+
+    def pack_data(self, data):
+        """Packing DVS or DAVIS data.
+
+        This function sends all outputs (assume everything is open),
+        if you only need to send part of the data,
+        simply override this function
+
+        # Arguments
+            data: tuple
+                The data to pack for sending.
+                The resulting data list is ordered.
+
+        # Returns
+            packed_data: list
+                the data to send.
+                DAVIS LIST ORDER:
+                    - polarity events
+                    - special events
+                    - frame timestamps
+                    - frames
+                    - imu events
+                The number of events are all omitted.
+
+                DVS LIST ORDER:
+                    - polarity events
+                    - special events
+        """
+
+        packed_data = [self.topic]
+        if len(data) == 8:
+            # DAVIS
+            packed_data += self.pack_np_array(data[0])  # polarity events
+            packed_data += self.pack_np_array(data[2])  # special events
+            packed_data += self.pack_np_array(data[4])  # frame time stamps
+            packed_data += self.pack_np_array(data[5])  # frames
+            packed_data += self.pack_np_array(data[6])  # imu events
+        else:
+            # DVS128
+            packed_data += self.pack_np_array(data[0])  # polarity events
+            packed_data += self.pack_np_array(data[2])  # special events
+
+        return packed_data
+
     def send_data(self):
         """Publish data main loop.
 
@@ -63,14 +128,20 @@ class EventPublisher(object):
         """
         while True:
             try:
-                data = self.device.get_data()
+                data = self.device.get_event()
+                if data is not None:
+                    data = self.pack_data(data)
 
-                data = self.process_data(data)
+                    data = self.process_data(data)
 
-                self.socket.send_multipart(
-                    [self.topic, data])
+                    self.socket.send_multipart(data)
             except KeyboardInterrupt:
+                self.close()
                 break
+
+    def close(self):
+        """Properly close the socket."""
+        self.device.shutdown()
 
 
 class EventSubscriber(object):
@@ -106,13 +177,86 @@ class EventSubscriber(object):
         """
         return data
 
+    def unpack_np_array(self, packed_data_array):
+        """Unpack a numpy array list from buffer.
+
+        # Arguments
+            packed_data_array: list
+                two element list, the first is a data array buffer,
+                the second is attributes to reconstruct array.
+        # Returns
+            unpacked_data_array: numpy.ndarray
+                the reconstructed array.
+        """
+        assert len(packed_data_array) == 2
+
+        try:
+            md = json.loads(packed_data_array[1])
+
+            buf_array = memoryview(packed_data_array[0])
+            return np.frombuffer(
+                buf_array, dtype=md['dtype']).reshape(md['shape'])
+        except Exception:
+            return None
+
+    def unpack_data(self, packed_data):
+        """Packing DVS or DAVIS data.
+
+        This function sends all outputs (assume everything is open),
+        if you only need to send part of the data,
+        simply override this function
+
+        # Arguments 
+            packed_data: list
+                the data to send.
+                DAVIS LIST ORDER:
+                    - polarity events
+                    - special events
+                    - frame timestamps
+                    - frames
+                    - imu events
+
+                DVS LIST ORDER:
+                    - polarity events
+                    - special events
+
+        # Arguments
+            unpacked_data: tuple
+                unpacked data
+                The resulting data list is ordered.
+        """
+
+        unpacked_data = [packed_data[0]]
+        if len(packed_data) == 11:
+            # DAVIS
+            unpacked_data.append(
+                self.unpack_np_array(packed_data[1:3]))  # polarity events
+            unpacked_data.append(
+                self.unpack_np_array(packed_data[3:5]))  # special events
+            unpacked_data.append(
+                self.unpack_np_array(packed_data[5:7]))  # frame time stamps
+            unpacked_data.append(
+                self.unpack_np_array(packed_data[7:9]))  # frames
+            unpacked_data.append(
+                self.unpack_np_array(packed_data[9:11]))  # imu events
+        else:
+            # DVS128
+            unpacked_data.append(
+                self.unpack_np_array(packed_data[1:3]))  # polarity events
+            unpacked_data.append(
+                self.unpack_np_array(packed_data[3:5]))  # special events
+
+        return unpacked_data
+
     def recv_data(self):
         """Subscribe data main loop.
 
         Reimplement to your need.
         """
         while True:
-            topic, data = self.socket.recv_multipart()
+            data = self.socket.recv_multipart()
+
+            data = self.unpacked_data(data)
 
             data = self.process_data(data)
 
