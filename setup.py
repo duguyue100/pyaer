72479473f7d2b2df3515c8becd8abcda197a5a3a
Yuhuang Hu <duguyue100@gmail.com>
2021-12-02 12:05:21 +0100

    bump version, fix classifier

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ Development Status :: 4 - Beta
 Intended Audience :: Science/Research
 Natural Language :: English
 Operating System :: OS Independent
-Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
